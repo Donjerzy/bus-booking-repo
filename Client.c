8a5244c4285768eebc75e8a066bbee1fdcf379df
Donjerzy <okoth46@gmail.com>
2022-05-20 10:28:12 +0300

    initializing client side bus booking functions

--- Client.c
+++ Client.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
+
+booking()
+{
+	return 0;
+}
+
+cancel(){
+	return 0;
+}
+
+details(){
+	return 0;
+}
+
 int main()
 {
-	printf("Project start :D");
+	int choice; // to store customer service choice
+	printf("Welcome to the bus booking section \n Enter 1: To book a seat 2: To cancel a seat 3:View booked ticket details \n");
+	scanf("%d", &choice);
+	if(choice == 1)
+	{
+		booking();
+	}else if(choice == 2)
+	{
+		cancel();
+	}else if(choice == 3)
+	{
+		details();
+	}
+	
 	return 0;
 }
