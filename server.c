d972ed29264ab9df1d9e2516341fd0819a9ad976
Donjerzy <okoth46@gmail.com>
2022-05-20 11:14:51 +0300

    added server connection to a client

--- server.c
+++ server.c
@@ -1,8 +1,48 @@
 #include<winsock2.h>
 #include <stdio.h>
 #include <stdlib.h>
+	WSADATA Winsockdata;
+	int iWsaStartup;
+	int iWsaCleanup;
+	SOCKET TCPServerSocket;
+	struct sockaddr_in TCPServerAdd;
+	struct sockaddr_in TCPClientAdd;
+	int iTCPClientAdd = sizeof(TCPClientAdd);
+	int iBind;
+	int iListen;
+	SOCKET sAcceptSocket;
+	int iSend;
+	int iRecv;
+	char RecvBuffer[512];
+	int iRecvBuffer = 50;
+	int iSenderBuffer = 512;
+	int iCloseSocket;
 
+	
+	
+
+	
 int main()
 {
-	return 0;
+	// WSAStartup 
+	iWsaStartup = WSAStartup(MAKEWORD(2, 2), &Winsockdata);
+	// filling in struct with address info of the server
+	TCPServerAdd.sin_family = AF_INET;
+	TCPServerAdd.sin_addr.s_addr = inet_addr("127.0.0.1");
+	TCPServerAdd.sin_port = htons(8000);
+	//Creating server socket
+	TCPServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	// binding the server address to the socket
+	iBind = bind(TCPServerSocket, (SOCKADDR*)&TCPServerAdd, sizeof(TCPServerAdd));
+	while(1)
+	{
+		iListen = listen(TCPServerSocket, 1);
+		printf("THE SERVER IS IN THE LISTENING STATE \n");
+		// accepting a connection
+		sAcceptSocket = accept(TCPServerSocket, (SOCKADDR*)&TCPClientAdd,&iTCPClientAdd);
+		printf("THE SERVER HAS ACCEPTED A NEW CONNECTION \n");
+		int new_sock = sAcceptSocket;
+		
+	}
+	
 }
