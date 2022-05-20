1b2597b651b91a159260c1438608c6220befe92e
Donjerzy <okoth46@gmail.com>
2022-05-20 11:14:11 +0300

    Added client connection to the server

--- Client.c
+++ Client.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include <winsock2.h>
 
+// initializing winsock and related variables
+	WSADATA Winsockdata;
+	int iWsaStartup;
+	int iWsaCleanup;
+	SOCKET TCPClientSocket;
+	int iCloseSocket;
+	struct sockaddr_in TCPServerAdd;
+	int iConnect;
+	int iSend;
+	int iRecv;
+	char RecvBuffer[512];
+	int iRecvBuffer = 512;
+	int iSenderBuffer = 50;
+	
+
+
+
+
 booking()
 {
 
@@ -25,6 +43,22 @@ details(){
 
 int main()
 {
+	// WSAStartup 
+	iWsaStartup = WSAStartup(MAKEWORD(2, 2), &Winsockdata);
+	// creating client socket
+	TCPClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	// filling socket structure with address info of the server
+	TCPServerAdd.sin_family = AF_INET;
+	TCPServerAdd.sin_addr.s_addr = inet_addr("127.0.0.1");
+	TCPServerAdd.sin_port = htons(8000);
+	//  Connection to server
+	iConnect = connect(TCPClientSocket, (SOCKADDR*)&TCPServerAdd, sizeof(TCPServerAdd));
+	printf("THE CLIENT HAS SUCCESSFULLY CONNECTED TO THE SERVER \n");
+	
+	
+	
+	
+	
 	int choice; // to store customer service choice
 	printf("Welcome to the bus booking section \n Enter 1: To book a seat 2: To cancel a seat 3:View booked ticket details \n");
 	scanf("%d", &choice);
