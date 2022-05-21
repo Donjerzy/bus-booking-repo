e5dd64c1ffefcc1e109a24532d149b7f2ef1b6f5
Donjerzy <okoth46@gmail.com>
2022-05-21 15:24:12 +0300

    iterative server complete

--- Client.c
+++ Client.c
@@ -27,7 +27,7 @@ booking()
 	char SenderBuffer[512] ="Book";
     //Sending Data to the server (booking)
 	iSend = send(TCPClientSocket, SenderBuffer, iSenderBuffer, 0);
-
+	tk:
 	printf("These are the seats in our buses. \n");
       printf(" [1]    [2]   | |    [Driver]\n");
 	printf("  ---------------------------------\n");
@@ -64,73 +64,22 @@ booking()
 	printf("Enter the seat you wish to book \n");
 	scanf("%d",&chair);
 	// sending selected book seats to the server
-	if(chair == 1){
-	char one[512] = "one";
-	iSend = send(TCPClientSocket, one, iSenderBuffer, 0);
-	}else if(chair == 2){
-	char two[512] = "two";
-	iSend = send(TCPClientSocket, two, iSenderBuffer, 0);
-	}else if(chair == 3){
-	char three[512] = "three";
-	iSend = send(TCPClientSocket, three, iSenderBuffer, 0);
-	}else if(chair == 4){
-	char four[512] = "four";
-	iSend = send(TCPClientSocket, four, iSenderBuffer, 0);
-	}else if(chair == 5){
-	char five[512] = "five";
-	iSend = send(TCPClientSocket, five, iSenderBuffer, 0);
-	}else if(chair == 6){
-	char six[512] = "six";
-	iSend = send(TCPClientSocket, six, iSenderBuffer, 0);
-	}else if(chair == 7){
-	char sev[512] = "seven";
-	iSend = send(TCPClientSocket, sev, iSenderBuffer, 0);
-	}else if(chair == 8){
-	char eig[512] = "eight";
-	iSend = send(TCPClientSocket, eig, iSenderBuffer, 0);
-	}else if(chair == 9){
-	char nine[512] = "nine";
-	iSend = send(TCPClientSocket, nine, iSenderBuffer, 0);
-	}else if(chair == 10){
-	char ten[512] = "ten";
-	iSend = send(TCPClientSocket, ten, iSenderBuffer, 0);
-	}else if(chair == 11){
-	char eleven[512] = "eleven";
-	iSend = send(TCPClientSocket, eleven, iSenderBuffer, 0);
-	}else if(chair == 12){
-	char twel[512] = "twelve";
-	iSend = send(TCPClientSocket, twel, iSenderBuffer, 0);
-	}else if(chair == 13){
-	char thir[512] = "thirteen";
-	iSend = send(TCPClientSocket, thir, iSenderBuffer, 0);
-	}else if(chair == 14){
-	char fourteen[512] = "fourteen";
-	iSend = send(TCPClientSocket, fourteen, iSenderBuffer, 0);
-	}else if(chair == 15){
-	char fif[512] = "fifteen";
-	iSend = send(TCPClientSocket, fif, iSenderBuffer, 0);
-	}else if(chair == 16){
-	char sixt[512] = "sixteen";
-	iSend = send(TCPClientSocket, sixt, iSenderBuffer, 0);
-	}else if(chair == 17){
-	char sevt[512] = "seventeen";
-	iSend = send(TCPClientSocket, sevt, iSenderBuffer, 0);
-	}else if(chair == 18){
-	char eighteen[512] = "eighteen";
-	iSend = send(TCPClientSocket,eighteen, iSenderBuffer, 0);
-	}else if(chair == 19){
-	char nineteen[512] = "nineteen";
-	iSend = send(TCPClientSocket,nineteen, iSenderBuffer, 0);
-	}else if(chair == 20){
-	char twen[512] = "twenty";
-	iSend = send(TCPClientSocket,twen, iSenderBuffer, 0);
-	}else if(chair == 21){
-	char twenty_one[512] = "twenty-one";
-	iSend = send(TCPClientSocket,twenty_one, iSenderBuffer, 0);
-	}else if(chair == 22){
-	char twenty_two[512] = "twenty-two";
-	iSend = send(TCPClientSocket,twenty_two, iSenderBuffer, 0);
+	char chairr[520];
+	memcpy(chairr, (char*)&chair,sizeof(unsigned int));
+	iSend = send(TCPClientSocket,chairr, iSenderBuffer, 0);
+	
+	//confirmation if seat is free
+	char conf[512];
+	iRecv = recv(TCPClientSocket,conf, iRecvBuffer, 0);
+	int co = strcmp(conf,"b");
+	if(co==0)
+	{
+		printf("The seat you have chosen has already been booked \n");
+		goto tk;
 	}
+	
+	
+	
 	printf("Enter your mobile number \n");
 	char no[9];
 	scanf("%s",no);
@@ -299,3 +248,4 @@ int main()
 	}
 	return 0;
 }
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bus-booking-repo
 Bus booking program developed following the client server paradigm with a concurrent connection oriented server! (C)
+winsock guide: https://docs.microsoft.com/en-us/windows/win32/winsock/getting-started-with-winsock
+
